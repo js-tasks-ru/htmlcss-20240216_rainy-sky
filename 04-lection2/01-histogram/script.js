a5f493e48357a1a8bfa4ec46dad084bd85afbb0a
rain84 <rainy_sky@mail.ru>
2024-03-05 22:59:02 +0300

    feat: 💄 added Histogram component

---
+++ 04-lection2/01-histogram/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const columns = document.querySelectorAll('.histogram__column');
+  columns.forEach((column) => {
+    column.style.height = `${Math.floor(Math.random() * 50 + 1)}px`;
+  });
+});
