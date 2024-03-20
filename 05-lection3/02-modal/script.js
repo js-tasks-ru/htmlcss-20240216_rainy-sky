7e8c79d98b315a27e09d2bc590c36905265a5bfe
rain84 <rainy_sky@mail.ru>
2024-03-20 17:32:23 +0300

    feat: ✨ added Modal component

---
+++ 05-lection3/02-modal/script.js
@@ -0,0 +1,35 @@
+const run = () => {
+  const $button = document.querySelector('#target');
+  const $cross = document.querySelector('.modal__cross');
+  const $overlay = document.querySelector('.modal__overlay');
+
+  if (!$cross || !$button) return;
+
+  $button.addEventListener('click', () => {
+    document.body.dataset.transition_open = true;
+    document.body.style.overflow = 'hidden';
+
+    setTimeout(() => {
+      document.body.classList.add('modal-open');
+      delete document.body.dataset.transition_open;
+    });
+  });
+
+  const onClose = () => {
+    document.body.dataset.transition_close = true;
+
+    const cb = () => {
+      delete document.body.dataset.transition_close;
+      document.body.style.overflow = '';
+      document.body.classList.remove('modal-open');
+      $overlay.removeEventListener('transitionend', cb);
+    };
+
+    $overlay.addEventListener('transitionend', cb);
+  };
+
+  $cross.addEventListener('click', onClose);
+  $overlay.addEventListener('click', onClose);
+};
+
+document.addEventListener('DOMContentLoaded', run);
